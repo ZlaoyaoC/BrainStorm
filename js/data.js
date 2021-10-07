bd1bb71b5117f68fc8b6bc293ca1fe31a6b52eb6
极丘 <57063095+ZlaoyaoC@users.noreply.github.com>
2021-10-07 20:42:10 +0800

    index.html
    dataJson

---
+++ js/data.js
@@ -0,0 +1,58 @@
+var dataJson = [
+    {
+        "root": "仓库",
+        "path": "跟随鼠标动",
+        "fileName": "1",
+        "btnName": "跟随鼠标动"
+    },
+    {
+        "root": "仓库",
+        "path": "滑动视差",
+        "fileName": "hdsc",
+        "btnName": "滑动视差"
+    },
+    {
+        "root": "仓库",
+        "path": "横向滚动",
+        "fileName": "实现鼠标滚轮横向滚动",
+        "btnName": "横向滚动"
+    },
+    {
+        "root": "仓库",
+        "path": "搜索框2",
+        "fileName": "ssk",
+        "btnName": "搜索框2"
+    },
+    {
+        "root": "仓库",
+        "path": "Css键盘",
+        "fileName": "jianpan",
+        "btnName": "Css键盘"
+    },
+    {
+        "root": "仓库",
+        "path": "3D切口名片",
+        "fileName": "3D切口名片",
+        "btnName": "3D切口名片"
+    },
+    {
+        "root": "仓库",
+        "path": "360°全景图",
+        "fileName": "qjt",
+        "btnName": "360°全景图"
+    },
+    {
+        "root": "仓库",
+        "path": "CSS3 新拟物风格 UI 元素",
+        "fileName": "index",
+        "btnName": "CSS3 新拟物风格 UI 元素"
+    },
+    {
+        "root": "仓库",
+        "path": "国庆大作业",
+        "fileName": "仿携程网移动端网页素材",
+        "btnName": "国庆大作业"
+    },
+];
+
+// <a href="./root/path/fileName.html">btnName</a>
